521ff2559e27bcc92594f76976a34e4e5261805a
maispace <joel@maispace.de>
2026-03-29 16:20:48 +0200

    [TASK] Apply bugfixes for use in conjunction with other extensions

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'ext-maispace-mai_faq' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mai_faq/Resources/Public/Icons/Extension.svg',
+    ],
+];
